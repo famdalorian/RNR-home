dc5537848b6a5ab29122f59024cbf6ecdf534ab9
Kevin Bell <famdalorian@proton.me>
2023-01-25 12:09:43 +0000

    added meet the chefs component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,12 +3,14 @@ import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
+import MeetChefs from "../pages/Meetchefs"
 
 function Home() {
   return (
     <>
       <HeroSection />
       <Cards />
+      <MeetChefs />
       <Footer />
     </>
   );
