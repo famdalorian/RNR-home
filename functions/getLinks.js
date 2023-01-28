cb8969d975d030c577314372e3e1b82556e4a9f0
Kevin Bell <famdalorian@proton.me>
2023-01-28 18:43:42 +0000

    running front end, need to continue to work on serverless functions/https://www.youtube.com/watch?v=73b1ZbmB96I~21:20/ JAM stack

---
+++ functions/getLinks.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+require("dotenv").config();
+exports.handler = async (event) => {
+  const GET_LINKS = `
+    query{
+        allLinks{
+            data{
+                name
+                _id
+                url
+                description
+                archived
+            }
+        }
+    }`;
+
+  const { data } = await axios({
+    url: "https://graphql.fauna.com/graphql",
+    method: "Post",
+    headers: {
+      Authorization: `Bearer ${process.env.FAUNA_SECRET_KEY}`,
+    },
+    data: {
+      query: GET_LINKS,
+      variables: {},
+    },
+  });
+  return { statusCode: 200,
+           body: JSON.stringify(data), 
+};
+};
